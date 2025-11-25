b7dabe3a334e3f86530b1c9e469910c4eeb60a5e
phanikumar-ravilisetti <kumarsfine1942@gmail.com>
2025-11-25 13:23:35 +0530

    img alt props updated

--- src/components/ChutneeHomeHeader/ChutneeHomeHeader.js
+++ src/components/ChutneeHomeHeader/ChutneeHomeHeader.js
@@ -4,18 +4,18 @@ const ChutneeHomeHeader = () => {
   return (
     <div className="main-header">
       <div className="logo-section">
-        <img src="https://res.cloudinary.com/dasuvkmgv/image/upload/v1763119556/Chutnee-logo-2-2048x1310_1_t2bizr.png" 
+        <img  alt="" src="https://res.cloudinary.com/dasuvkmgv/image/upload/v1763119556/Chutnee-logo-2-2048x1310_1_t2bizr.png" 
         className="logo-circle" />
-        <img src="https://res.cloudinary.com/dasuvkmgv/image/upload/v1763119562/Chutnee-logo-2-2048x1310_2_j0e9p9.png" className="logo-text" />
+        <img alt="" src="https://res.cloudinary.com/dasuvkmgv/image/upload/v1763119562/Chutnee-logo-2-2048x1310_2_j0e9p9.png" className="logo-text" />
       </div>
       <div className="header-right-section">
       <nav className="nav-links">
-        <a href="">OUR STORY</a>
-        <a href="">MENU</a>
-        <a href="">AFTERNOON TEA</a>
-        <a href="">EVENTS</a>
-        <a href="">CHUTNEE FAMILY</a>
-        <a href="">CONTACT</a>
+        <button className="anchor-buttons" type="button">OUR STORY</button>
+        <button className="anchor-buttons" type="button">MENU</button>
+        <button className="anchor-buttons" type="button">AFTERNOON TEA</button>
+        <button className="anchor-buttons" type="button">EVENTS</button>
+        <button className="anchor-buttons" type="button">CHUTNEE FAMILY</button>
+        <button className="anchor-buttons" type="button">CONTACT</button>
       </nav>
         <button className="reservation-btn">RESERVATION <span className="span-arrow">{">"}</span></button>
       </div>
